47f4677363ce73295af91e2d5ede0cab85990750
TheJerryChang <y.chang@se18.qmul.ac.uk>
2020-11-16 21:16:13 +0800

    LVL2 boss and enemies

---
+++ Assets/Prefab/Team member/Jerry/WindMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindMove : MonoBehaviour
+{
+    private float speed = 2f;
+    public Rigidbody2D rb;
+    
+    void Start()
+    {
+        
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        
+        rb.velocity = transform.right * -speed;
+    }
+
+
+    void OnTriggerEnter2D(Collider2D hitted){
+        PlayerController playermovement = hitted.GetComponent<PlayerController>();
+        PlayerStat playerstat = hitted.GetComponent<PlayerStat>();
+        if(playermovement != null){
+            playermovement.SendMessage("setMoving", false);
+            playerstat.SendMessage("decreaseHP", 10);
+        }
+        Destroy(gameObject);
+    }
+
+}
