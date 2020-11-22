339d0ed9a47dad335bb3699be55f0b1cb344fa9d
JerryTheChang <a13838734777@gmail.com>
2020-11-22 16:05:35 +0800

    traps done

---
+++ Assets/Scripts/Trap/Lava/Lava.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+    //private PlayerStat stats;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //stats = GameObject.Find("Player").GetComponent<PlayerStat>();
+    }
+
+    //while player touches the lava, kill the player
+    void OnCollisionEnter2D(Collision2D collides){
+        
+        if(collides.gameObject.name.Equals("Player")){
+            //stats.SendMessage("decreaseHP", 10);
+            Destroy(collides.gameObject);
+        }
+        
+    }
+}
